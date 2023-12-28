6efeca6d12ed607130a4067d8c9eb146d48bbd2e
Saran012 <saran0012@gmail.com>
2023-12-28 08:59:39 +0530

    header section

---
+++ src/components/HeroSection/HeroSection.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styles from "./HeroSection.module.css"
+import { ReactComponent as Mailicon } from "../../assets/mailIcon.svg";
+import Heroimage from "../../assets/heroimage.svg"
+
+
+
+export default function HeroSection (){
+
+    const onSubmit =(e)=>{
+      e.preventDefault();
+    }
+
+
+  return (
+    <div className={styles.herowrapper}>
+       <div className={styles.heading}>
+            <p className={styles.first}>You don’t have to </p>
+            <p className={styles.second}>Fight them Alone.</p>
+            <p className={styles.para}>Lorem ipsum dolor sit amet consectetur adipiscing elit blandit,</p>
+            <p className={styles.para}>curabitur sodales conubia ut inceptos faucibus himenaeos tortor</p>
+            <p className={styles.para}> eget, hac massa gravida arcu interdum proin curae.</p>
+            <div className={styles.emailSection}>
+                <input className={styles.input}  placeholder="Enter your eamil address" />
+                <button className={styles.talk}> Let's Talk</button>
+            </div>
+        </div>
+        <div className={styles.imgConatiner}>
+           <img className={styles.img} src={Heroimage}/>
+        </div>
+    </div>
+  )
+}
---
+++ src/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./NavigationBar.module.css"
+import { ReactComponent as Logo } from "../../assets/logo.svg";
+
+
+
+export default function NavigationBar(){
+    return(
+        <div className={styles.wrapper}>
+            <div className={styles.logo}>
+              <Logo/>
+            </div>
+         
+           <div className={styles.category}>
+              <a href="#"><div>Home</div></a>
+              <a href="#"><div>Attorneys</div></a>
+              <a href="#"><div>Practice Areas</div></a>
+              <a href="#"><div>About Us</div></a>
+           </div>
+
+              <button className={styles.contactButton}>Contact Now</button>
+
+        </div>
+    )
+}
